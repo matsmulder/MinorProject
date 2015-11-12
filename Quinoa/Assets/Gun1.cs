ee8f74a342f50833961ce7d4850bf78095e1880e
unknown <bugattid@gmail.com>
2015-11-12 16:03:32 +0100

    Het hele core gameplay project

---
+++ Quinoa/Assets/Gun1.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gun1 : MonoBehaviour {
+    private Rigidbody rb; //the 3D model of the gun as rigidbody
+    public Rigidbody prefabBullet; //the 3D model of the bullet as rigidbody
+    public float bulletSpeed; //the travelling speed of the bullet
+    private Vector3 bulletDir;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire1")) //PLACEHOLDER, to be changed to left mouse button; left mouse button is still buggy
+        {
+            //StartCoroutine(bullet());
+            //instantiate bullet
+            Rigidbody clone = Instantiate(prefabBullet, new Vector3(rb.position.x,rb.position.y,rb.position.z + 2), cameraMovement.tf.rotation) as Rigidbody;
+
+            Ray shootDir = Camera.main.ScreenPointToRay(Input.mousePosition); //a ray in the looking direction
+            bulletDir = shootDir.direction;
+
+            clone.velocity = shootDir.direction;
+
+
+        }
+	}
+
+    IEnumerator bullet()
+    {
+        yield return new WaitForSeconds(1);
+    }
+}
---
+++ Quinoa/Assets/Scripts/cameraMovement.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraMovement : MonoBehaviour
+{
+
+    public GameObject player; // player GameObject to allow interaction of camera and player
+    public static Transform tf;
+    private Vector3 offset;
+
+    private float rotationZ = 0f;
+    private float sensitivityZ = 2f;
+
+    // Use this for initialization
+    void Start()
+    {
+        tf = GetComponent<Transform>();
+        offset = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + offset;
+
+        if (transform.rotation.eulerAngles.x < 90 || (transform.rotation.eulerAngles.x > 270 && transform.rotation.eulerAngles.x <= 360) || ((transform.rotation.eulerAngles.x == 90) && (playerMovement.mouseMovementY >= 0)) || ((transform.rotation.eulerAngles.x == 270) && (playerMovement.mouseMovementY <= 0))) //clamp rotation to avoid upside-down view
+        { 
+            transform.Rotate(new Vector3(-1 * playerMovement.mouseMovementY * Time.deltaTime * playerMovement.sensitivity, 0, 0), Space.Self); // rotate the camera when moving the mouse up and down
+        }
+
+        if(transform.rotation.eulerAngles.x == 90)
+        {
+
+        }
+    }
+
+}
