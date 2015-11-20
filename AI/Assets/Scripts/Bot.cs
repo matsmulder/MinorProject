ac27ab9b11ddb8acd610bc8494f1335faaca6ea9
HugoReijm <itsanickel@yahoo.com>
2015-11-20 17:35:00 +0100

    AI beginnings <feature>

---
+++ AI/Assets/Scripts/Bot.cs
@@ -0,0 +1,154 @@
+﻿using UnityEngine;
+//using UnityEngine.Networking;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+
+public class Bot : MonoBehaviour{
+    private float fieldofViewAngle = 110f;
+    private bool playerInSight;
+
+    private int health;
+    private int ammo;
+    private bool dead;
+    private Rigidbody rb;
+    private int team;
+    private List<Vector3> points;
+    private Calculator calculator;
+    private double l = 6;
+    private SphereCollider col;
+    //https://unity3d.com/learn/tutorials/projects/stealth/enemy-sight
+
+    public void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        col = GetComponent<SphereCollider>();
+        dead = false;
+        health = 100;
+        calculator = Calculator.getCalculator(); 
+    }
+
+    public double LSV(Vector3 point)
+    {
+        double PLVS = 0;
+        double HLVS = 0;
+        double ALVS = 0;
+        double l = 4;
+        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+        GameObject[] bots = GameObject.FindGameObjectsWithTag("Bot");
+        GameObject[] healths = GameObject.FindGameObjectsWithTag("Health");
+        GameObject[] ammos = GameObject.FindGameObjectsWithTag("Ammo");
+
+        foreach (var obj in healths)
+        {
+            HLVS = +Math.Exp(-Math.Pow(Vector3.Distance(point, obj.transform.position), 2)) / (health + 1);
+        }
+
+        foreach (var obj in ammos)
+        {
+            ALVS = +Math.Exp(-Math.Pow(Vector3.Distance(point, obj.transform.position), 2)) / (ammo + 1);
+        }
+
+        foreach (var obj in players)
+        {
+            PLVS = + Math.Exp(-Math.Pow(Vector3.Distance(point, obj.transform.position) / l, 2));
+        }
+        foreach (var obj in bots)
+        {
+            PLVS = + Math.Exp(-Math.Pow(Vector3.Distance(point, obj.transform.position) / l, 2));
+        }
+        //((-obj.getTeam() * team + 1) / 2) *
+        double c1 = 0.33;
+        double c2 = 0.33;
+        double c3 = 0.33;
+        return c1 * PLVS + c2 * HLVS + c3 * ALVS;
+    }
+
+    public double SV(Vector3 point)
+    {
+        return calculator.getGVSconstant() * calculator.getGSV(point) + calculator.getGVSconstant() * LSV(point);
+    }
+
+    public void Update()
+    {
+        transform.Translate(new Vector3(1*Time.deltaTime, 0, 0));
+        points = new List<Vector3>();
+        foreach(Vector3 obj in calculator.getMap())
+        {
+            if(Vector3.Distance(this.transform.position,obj)<l)
+            {
+                if (Vector3.Angle(this.transform.forward, obj) < fieldofViewAngle / 2)
+                {
+                    points.Add(obj);
+                }
+            }
+        }
+
+        double index = 0;
+        Vector3 bestPoint = new Vector3(0,0,0);
+        foreach(Vector3 point in points)
+        {
+            double tempSV = SV(point);
+            if (index<tempSV)
+            {
+                index = tempSV;
+                bestPoint = point;
+            }
+        }
+
+        MoveBot(bestPoint);
+    }
+
+    public void MoveBot(Vector3 point)
+    {
+        transform.Rotate((point - new Vector3(transform.rotation.x,transform.rotation.y,transform.rotation.z)) * Time.deltaTime);
+        transform.Translate((point - transform.position) * Time.deltaTime);
+    }
+
+    public void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.CompareTag("Bot")|| other.gameObject.CompareTag("Player"))
+        {
+            playerInSight = false;
+            //if ((-other.gameObject.getTeam()*team+1)/2 == 1)
+            //{
+                Vector3 direction = other.transform.position - transform.position;
+                float angle = Vector3.Angle(direction, transform.forward);
+                if (angle < fieldofViewAngle / 2)
+                {
+                    RaycastHit hit;
+                    if (Physics.Raycast(transform.position + transform.up, direction.normalized, out hit, col.radius))
+                    {
+                        if (hit.collider.tag=="Bot" || hit.collider.tag=="Player")
+                        {
+                            playerInSight = true;
+                            rb.transform.Translate(new Vector3(1,1,1));
+                        }
+                    }
+                }
+            //}
+        }
+    }
+
+    //public void Shoot()
+    //{
+    //    while(playerInSight)
+    //    {
+    //        transform.Rotate(Vector3.Angle( , transform.forward));
+    //        Fire();
+    //    }
+    //}
+
+    public void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.CompareTag("Bot")|| other.gameObject.CompareTag("Player"))
+        {
+            playerInSight = false;
+        }
+    }
+
+    public int getTeam()
+    {
+        return team;
+    }
+}
---
+++ AI/Assets/Scripts/Calculator.cs
@@ -0,0 +1,107 @@
+﻿using UnityEngine;
+//using UnityEngine.Networking;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.Runtime.CompilerServices;
+
+public class Calculator:MonoBehaviour
+{
+    private static Calculator singleton=null;
+    private static List<Vector3> map;
+
+    private static double c1 = 0.5;
+    private static double c2 = 0.5;
+    private static double[] mapGSV;
+    public static double lengthConst = 2;
+
+    private Calculator()
+    {
+        map = makeMap();
+        mapGSV = new double[map.Count];
+        for (int i = 0; i != mapGSV.Length; i++)
+        {
+            mapGSV[i] = GSV(map[i]);
+        }
+    }
+
+    [MethodImpl(MethodImplOptions.Synchronized)]
+    public static Calculator getCalculator()
+    {
+        if(singleton==null)
+        {
+            singleton = new Calculator();
+        }
+        return singleton;
+    }
+
+    public List<Vector3> getMap()
+    {
+        return map;
+    }
+
+    private List<Vector3> makeMap()
+    {
+        List<Vector3> points = new List<Vector3>();
+        GameObject[] floors = GameObject.FindGameObjectsWithTag("Floor");
+        double stepsize = 0.125;
+        foreach(GameObject obj in floors)
+        {
+            for (double i = -obj.transform.lossyScale.x / 2; i <= obj.transform.lossyScale.x / 2; i = i + stepsize) 
+            {
+                for (double j = -obj.transform.lossyScale.z / 2; j <= obj.transform.lossyScale.z / 2; j = j + stepsize) 
+                {
+                    points.Add(new Vector3((float)(i + obj.transform.position.x), obj.transform.position.y, (float)(j + obj.transform.position.z)));
+                }
+            }
+        }
+        return points;
+    }
+
+    public double GSV(Vector3 point)
+    {
+        GameObject[] walls = GameObject.FindGameObjectsWithTag("Wall");
+        double DGVS = 0;
+        double OGVS = 0;
+        double l = lengthConst;
+        foreach (GameObject obj in walls)
+        {
+            Vector3 wallCenter = new Vector3(obj.transform.position.x, obj.transform.position.y - obj.transform.lossyScale.y / 2, obj.transform.position.z);
+            double stepSize = 0.125;
+            Vector3 closestPoint = wallCenter;
+            double index = Vector3.Distance(wallCenter, point);
+            double min = Math.Min(obj.transform.lossyScale.x,obj.transform.lossyScale.z);
+            double max = Math.Max(obj.transform.lossyScale.x, obj.transform.lossyScale.z);
+            for (double t = -1; t <= 1; t = t + stepSize)
+            {
+                Vector3 wallPos = wallCenter + new Vector3((float)(((max - min) * Math.Cos(obj.transform.rotation.y * Math.PI / 180) + min) * t / 2), 0, (float)(((max - min) * Math.Sin(obj.transform.rotation.y * Math.PI / 180) + min) * t / 2));
+                
+                if (Vector3.Distance(wallPos, point) < Vector3.Distance(closestPoint, point))
+                {
+                    closestPoint = wallPos;
+                    index = Vector3.Distance(wallPos, point);
+                }
+            }
+            DGVS = +(obj.transform.position.y + obj.transform.lossyScale.y / 2) * Math.Exp(-Math.Pow(index / l,2));
+            OGVS = +Math.Exp(-Math.Pow(index / (obj.transform.position.y + obj.transform.lossyScale.y / 2),2));
+        }
+        double c1 = 0.5;
+        double c2 = 0.5;
+        return c1 * DGVS + c2 * (-OGVS + 4);
+    }
+
+    public double getGSV(Vector3 point)
+    {
+        return mapGSV[map.IndexOf(point)];
+    }
+
+    public double getGVSconstant()
+    {
+        return c1;
+    }
+
+    public double getLVSconstant()
+    {
+        return c2;
+    }
+}
