7cc2f10a84d0457ac1a08ad12a878323bf047141
unknown <bugattid@gmail.com>
2015-11-22 17:44:27 +0100

    geluidseffect toegevoegd bij het schieten. De eerdere poging tot het tekenen van een lijn met een LineRenderer is geschrapt; deze maakt plaats voor een andere manier om een lijn te tekenen welke nog niet geimplementeerd is maar zeer waarschijnlijk in de volgende commit voorkomt.

---
+++ Quinoa Photon/Assets/FXmanager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FXmanager : MonoBehaviour {
+
+    public AudioClip sniperSound;
+    //public AudioClip sniperRicochet;
+
+    void Start()
+    {
+       // audio = GetComponent<AudioSource>();
+    }
+
+    [PunRPC]
+    void SniperBulletFX( Vector3 startPos, Vector3 endPos)
+    {
+        AudioSource.PlayClipAtPoint(sniperSound, startPos);
+    }
+
+
+
+
+}
--- Quinoa Photon/Assets/playerShooting.cs
+++ Quinoa Photon/Assets/playerShooting.cs
@@ -6,12 +6,23 @@ public class playerShooting : MonoBehaviour {
     public float fireRate;
     private bool fireFlag;
     public float damage;
-    public LineRenderer laserBeam;
+    //public LineRenderer laserBeam;
+    public float shootingRange;
 
-	// Use this for initialization
+    // Use this for initialization
+
+    FXmanager fxManager;
+
+
 	void Start () {
         fireFlag = true;
-        laserBeam = GetComponent<LineRenderer>();
+        fxManager = GameObject.FindObjectOfType<FXmanager>();
+
+        if(fxManager == null)
+        {
+            Debug.Log("no fxmanager found");
+        }
+        //laserBeam = GetComponent<LineRenderer>();
 	}
 	
 	// Update is called once per frame
@@ -40,12 +51,12 @@ public class playerShooting : MonoBehaviour {
         hitTransform = FindClosestHitInfo(ray, out hitPoint);
 
        
-        laserBeam.SetPosition(0, transform.position); // position of the player
+        //laserBeam.SetPosition(0, transform.position); // position of the player
 
         if(hitTransform != null)
         {
-            laserBeam.SetPosition(1, hitTransform.position); //set the target point of the laser beam to the transform of the nearest hit point
-            Debug.Log(hitTransform.name);
+            //laserBeam.SetPosition(1, hitTransform.position); //set the target point of the laser beam to the transform of the nearest hit point
+            //Debug.Log(hitTransform.name);
             Health h = hitTransform.GetComponent<Health>();
 
             while(h == null && hitTransform.parent) //cycle through childs until the parent which holds the Health script is found
@@ -59,17 +70,29 @@ public class playerShooting : MonoBehaviour {
                 h.GetComponent<PhotonView>().RPC("TakeDamage",PhotonTargets.All, damage);
                 //this line is the equivalent of h.TakeDamage(damage) but synchronized
             }
+
+            if(fxManager != null)
+            {
+                fxManager.GetComponent<PhotonView>().RPC("SniperBulletFX", PhotonTargets.All, Camera.main.transform.position, hitPoint);
+            }
+        }
+        else
+        {
+            //nothing is hit
+            if (fxManager != null)
+            {
+                fxManager.GetComponent<PhotonView>().RPC("SniperBulletFX", PhotonTargets.All, Camera.main.transform.position, Camera.main.transform.forward * shootingRange);
+            }
         }
 
 
-        laserBeam.enabled = true; //enable visibility of laser beam
+        //laserBeam.enabled = true; //enable visibility of laser beam
         ////////////////////////////////
-        Debug.Log("shoot");
         yield return new WaitForSeconds(fireRate);
         fireFlag = true;
         ///////////////////////////////
-        laserBeam.enabled = false; //disable visibility of laser beam
-        laserBeam.SetPosition(1, transform.position); //reset beam by taking the player position as second position
+        //laserBeam.enabled = false; //disable visibility of laser beam
+        //laserBeam.SetPosition(1, transform.position); //reset beam by taking the player position as second position
     }
 
     Transform FindClosestHitInfo(Ray ray, out Vector3 hitPoint)
@@ -85,11 +108,21 @@ public class playerShooting : MonoBehaviour {
             if(hit.transform != this.transform && (closestHit == null || hit.distance < distance))
             {
                 //something is hit that is the first thing and not us
+                
                 closestHit = hit.transform;
+
                 distance = hit.distance;
                 hitPoint = hit.point;
+
             }
         }
+        //set end of laser beam to the point of impact
+        if(closestHit == null)
+        {
+            Debug.Log("nullshoot");
+            hitPoint = Camera.main.transform.forward * shootingRange;
+        }
+        //laserBeam.SetPosition(1, hitPoint);
 
         return closestHit;
     }
