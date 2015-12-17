90a472a57194ae748575e19836ffc02c081d6d6b
Rick <rick_molenaar96@live.nl>
2015-12-17 15:00:56 +0100

    Working facebook posting. Check function description for usage.

--- Project Quinoa/Assets/FacebookManager.cs
+++ Project Quinoa/Assets/FacebookManager.cs
@@ -3,13 +3,38 @@ using System.Collections;
 
 public class FacebookManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    private static string FACEBOOK_URL = "http://www.facebook.com/dialog/feed";
+    private static string FACEBOOK_APP_ID = "472366919613458";
+
+    // Use this for initialization
+    void Start () {
         DontDestroyOnLoad(this);
-	}
+    }
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+    static void ShareToFacebook(string linkParameter, string nameParameter, string captionParameter, string descriptionParameter, string pictureParameter, string redirectParameter)
+    {
+        /* PARAMETERS:
+        * link: Link behind post (when someone clicks on the post)
+        * name: Name of the post
+        * caption: text below the name, should be descriptive
+        * description: footer text, should be short
+        * picture: link to picture
+        * redirect: link to send user to after posting. Just use https://www.facebook.com/ in general
+        */
+        Application.OpenURL(FACEBOOK_URL + "?app_id=" + FACEBOOK_APP_ID +
+        "&link=" + WWW.EscapeURL(linkParameter) +
+        "&name=" + WWW.EscapeURL(nameParameter) +
+        "&caption=" + WWW.EscapeURL(captionParameter) +
+        "&description=" + WWW.EscapeURL(descriptionParameter) +
+        "&picture=" + WWW.EscapeURL(pictureParameter) +
+        "&redirect_uri=" + WWW.EscapeURL(redirectParameter));
+    }
+
+
+
 }
