1d745ef2b13037a1c7aadaea45d03aad7daa51eb
laurensWe <laurensweijs@hotmail.com>
2016-01-10 11:00:44 +0100

    minor bug fixes for the test phase of the game.

--- Project Quinoa/Assets/Scripts/Gameplay/NoScope.cs
+++ Project Quinoa/Assets/Scripts/Gameplay/NoScope.cs
@@ -5,9 +5,17 @@ public class NoScope : MonoBehaviour {
 
     public int zoomFOV;
     public int normalFOV;
+
+	Texture crosshairTexture;
+	Rect crosshairRect;
+	private bool isTrigger = true;
+
 	// Use this for initialization
 	void Start () {
-	
+		float crosshairSize = Screen.width * 0.1f;
+		crosshairTexture = Resources.Load ("Textures/crosshair") as Texture;
+		crosshairRect = new Rect (Screen.width / 2 - crosshairSize / 2, Screen.height / 2 - crosshairSize / 2,
+		                          crosshairSize, crosshairSize); 
 	}
 	
 	// Update is called once per frame
@@ -15,10 +23,18 @@ public class NoScope : MonoBehaviour {
         if (Input.GetButton("Fire2"))
         {
             Camera.main.fieldOfView = zoomFOV;
+			isTrigger = false;
         }
         if(Input.GetButtonUp("Fire2"))
         {
             Camera.main.fieldOfView = normalFOV;
+			isTrigger = true;
         }
+	}
+
+	void onGui(){
+		if (isTrigger) {
+			GUI.DrawTexture(crosshairRect,crosshairTexture);
+		}
 	}
 }
