83f1fab15b4d216fabc93021ffb02346d7717cc6
laurensWe <laurensweijs@hotmail.com>
2016-01-11 15:15:12 +0100

    Http request and response made in C#. Data gathering will be done when you guys are ready.

--- Server & Database stuff/DataController.cs
+++ Server & Database stuff/DataController.cs
@@ -1,15 +1,50 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.IO;
 
-public class DataController : MonoBehaviour {
+public class DataController{
+	public string playerName;
+	public int kills;
+	public int deaths;
+	public int won;
+	public int lost;
 
-	// Use this for initialization
-	void Start () {
-	
+	public void sentDBData(){
+		// initialisation
+		playerJson DBJSON = new playerJson (playerName, kills, deaths, won, lost);
+		String json = DBObject.toJson();
+
+		// ini web
+		var httpWebRequest = (HttpWebRequest)WebRequest.Create ("https://drproject.twi.tudelft.nl:8082/postStats");
+		httpWebRequest.ContentType = "text/json";
+		httpWebRequest.Method = "POST";
+
+		// write away to the node.js 
+		var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream();
+		streamWriter.Write(json);
+		streamWriter.Flush();
+		streamWriter.Close();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	public void getDBData(string name){
+		// ini web
+		var httpWebRequest = (HttpWebRequest)WebRequest.Create ("https://drproject.twi.tudelft.nl:8082/getStats");
+		httpWebRequest.ContentType = "text/json";
+		httpWebRequest.Method = "POST";
+
+		// write away to the node.js 
+		var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream());
+		string json = "{\"user\":" + name + "};";
+		streamWriter.Write(json);
+		streamWriter.Flush();
+		streamWriter.Close();
+
+		// ini res
+		var httpResponse = (HttpResponse)httpWebRequest.GetResponse ();
+		var streamReader = new StreamReader (httpResponse.GetResponseStream ());
+		string userStats = streamReader.ReadToEnd;
+
+		// handle the userStats data
+
 	}
 }
